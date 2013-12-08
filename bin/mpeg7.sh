fc9769a0eb7e86800a6174d1fab63425993a8b7b
Martin Štekl <martin.stekl@gmail.com>
2013-12-08 02:34:16 +0100

    Fixed command for MPEG7 conversion

--- bin/mpeg7.sh
+++ bin/mpeg7.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -Xmx4G -jar MPEG7AudioEnc-0.4-rc3.jar $1 ./MPEG7.config.xml > $2
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+java -Xmx4G -jar $DIR/MPEG7AudioEnc-0.4-rc3.jar $1 $DIR/MPEG7.config.xml > $2
